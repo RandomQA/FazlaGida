8119222d1ad24ee4565b265155d10a241ad3573f
alitechproed <acan.techproed@gmail.com>
2021-06-13 12:28:47 +0300

    sonV

--- src/test/java/runners/FazlaGidaRunner.java
+++ src/test/java/runners/FazlaGidaRunner.java
@@ -12,9 +12,9 @@ import org.junit.runner.RunWith;
 
         features = "src/test/resources/features",
         glue = "stepdefinitions",
-        tags = "@kisiselBilgiler",
+        tags = "@Uzman",
         dryRun = false
 )
-public class TutumluAnnelerRunner {
+public class FazlaGidaRunner {
 
 }
